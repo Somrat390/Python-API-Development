62522ef811ee4c3e9559eccee068b46ba37f0bdd
Somrat <somratsheikh426@gmail.com>
2025-09-03 21:45:30 +0600

    response status code

---
+++ curd.py
@@ -0,0 +1,50 @@
+from typing import Optional
+from fastapi import Body, FastAPI, Response, status
+from pydantic import BaseModel
+from random import randrange
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True 
+    rating: Optional[int] = None 
+
+
+my_posts = [{"title":"title of post 1", "content": "content of post 1","id": 1},{"title":"favourite food", "content": "I like pizza","id": 2}]
+
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to my api!!"}
+
+@app.get("/posts")
+def get_posts():
+    return {'data': my_posts}
+
+
+@app.post('/posts')
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0,1000000)
+    my_posts.append(post_dict) 
+    return {"data": post_dict}
+
+@app.get("/latest")
+def get_latest_post():
+    return {"detail": my_posts[-1]}
+
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post =find_post(id)
+    if not post:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": f"post with id: {id} was not found"}
+    return {"post details": post}
+
